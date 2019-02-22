92b32d7131e135dae32da19f1476470246b8d017
Alex Marin <alex.marin89@gmail.com>
2019-02-22 16:12:53 -0500

    Build basic unit tests with CMake

---
+++ unit_tests/flat_hash_map_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(flat_hash_map, basic)
+{
+    int x = 2;
+    EXPECT_EQ(x, 1);
+}
+
---
+++ unit_tests/unordered_map_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(unordered_map, basic)
+{
+    int x = 1;
+    EXPECT_EQ(x, 1);
+}
+
