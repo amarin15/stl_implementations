d4f32d9291b0b90dc6da90fd8b727df77f7f00ef
Alex Marin <alex.marin89@gmail.com>
2019-02-26 17:03:14 -0500

    implement tuple basic functionality

--- include/si_tuple.h
+++ include/si_tuple.h
@@ -1,10 +1,74 @@
 #ifndef SI_TUPLE_H
 #define SI_TUPLE_H
 
+#include <utility> // std::forward
+
 namespace si {
 
-class tuple{
+// If we wanted to implement an iterative version of tuple instead,
+// we would have used multiple inheritance instead of recursion.
+// http://mitchnull.blogspot.com/2012/06/c11-tuple-implementation-details-part-1.html
+
+
+// Each member of a tuple will have its value stored in a leaf.
+// The non-type template parameter `N` allows the `get` function
+// to find the value in O(1) time.
+template<size_t N, typename T>
+struct tuple_leaf
+{
+    T value;
+
+    tuple_leaf(T&& t)
+        : value(std::forward<T>(t))
+    {}
 };
+
+// `tuple_impl` is a proxy for the `tuple` class that has an extra
+// non-type template parameter `N`.
+template <size_t N, typename ... Ts>
+struct tuple_impl;
+
+// Base case: empty tuple.
+template <size_t N>
+struct tuple_impl<N>
+{};
+
+// Recursive specialization.
+template <size_t N, typename HeadT, typename ... TailTs>
+struct tuple_impl<N, HeadT, TailTs...>
+    : tuple_leaf<N, HeadT>         // inherit the N-th leaf value.
+    , tuple_impl<N + 1, TailTs...> // define recursively.
+{
+    tuple_impl<N, HeadT, TailTs...>(HeadT&& head, TailTs&&... tail)
+        : tuple_leaf<N, HeadT>(std::forward<HeadT>(head))
+        , tuple_impl<N + 1, TailTs...>(std::forward<TailTs>(tail)...)
+    {
+    }
+};
+
+// Define tuple as a more convenient alias of tuple_impl.
+template <typename ... Ts>
+using tuple = tuple_impl<0, Ts...>;
+
+
+// Define our get function to obtain the N-th item in a tuple.
+template<size_t N, typename HeadT, typename ... TailTs>
+HeadT& get(tuple_impl<N, HeadT, TailTs...>& tpl)
+{
+    // Fully qualified name for the member, to find the right one.
+    return tpl.tuple_leaf<N, HeadT>::value;
+}
+
+
+// Define our make_tuple function.
+template <typename ... Ts>
+// `args` is a function parameter of a function template declared as
+// an rvalue reference to a cv-unqualified type template parameter of
+// the same function template, which makes it a forwarding reference.
+tuple<Ts...> make_tuple(Ts&&... args)
+{
+    return tuple<Ts...>(std::forward<Ts>(args)...);
+}
 
 } // namespace si
 
--- unit_tests/tuple_test.cpp
+++ unit_tests/tuple_test.cpp
@@ -26,87 +26,97 @@
 
 // Member functions
 
-TEST(si_tuple, assigment_operator)
+/*
+TEST(si_tuple, assignment_operator)
 {
     // Given
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
+    //si::tuple<int, std::string, double> tpl = si::make_tuple(1, std::string("2"), 3.45);
 
     // When
-    auto tpl_copy = tpl;
+    //auto tpl_copy = tpl;
 
     // Then
-    EXPECT_EQ(tpl_copy, tpl);
+    //EXPECT_EQ(tpl_copy, tpl);
 }
 
 TEST(si_tuple, swap)
 {
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
+    auto tpl = si::make_tuple(1, "2", 3.45);
     auto tpl_copy = tpl;
-    std::get<0>(tpl_copy) = 10;
+    si::get<0>(tpl_copy) = 10;
 
     tpl.swap(tpl_copy);
 
-    EXPECT_EQ(std::get<0>(tpl), 10);
-    EXPECT_EQ(std::get<0>(tpl_copy), 1);
+    EXPECT_EQ(si::get<0>(tpl), 10);
+    EXPECT_EQ(si::get<0>(tpl_copy), 1);
 }
-
+*/
 
 // Non-member functions
 
 TEST(si_tuple, make_tuple)
 {
-    auto tpl_char_ptr = std::make_tuple(1, "2", std::string("3.45"));
-    std::tuple<int, std::string, std::string> tpl_string = std::make_tuple(1, "2", "3.45");
+    {
+        auto tpl = si::make_tuple(1, std::string("2"));
 
-    EXPECT_TRUE((std::is_same<decltype(tpl_char_ptr), std::tuple<int, const char *, std::string>>::value));
-    EXPECT_TRUE((std::is_same<decltype(tpl_string),   std::tuple<int, std::string,  std::string>>::value));
+        EXPECT_TRUE((std::is_same<decltype(tpl),
+                                  si::tuple<int, std::string>>::value));
+        EXPECT_EQ(si::get<0>(tpl), 1);
+        EXPECT_EQ(si::get<1>(tpl), std::string("2"));
+    }
+
+    {
+        std::string str = "2";
+        auto tpl = si::make_tuple(1.0, str);
+
+        EXPECT_TRUE((std::is_same<decltype(tpl),
+                                  si::tuple<double, std::string&>>::value));
+        EXPECT_EQ(si::get<1>(tpl), str);
+    }
 }
 
 TEST(si_tuple, get)
 {
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
+    si::tuple<int, double> tpl = si::make_tuple(1, 2.34);
 
-    auto &a = std::get<0>(tpl);
-    auto &b = std::get<1>(tpl);
-    auto &c = std::get<2>(tpl);
+    auto &a = si::get<0>(tpl);
+    auto &b = si::get<1>(tpl);
 
-    EXPECT_TRUE((std::is_same<decltype(a), int&        >::value));
-    EXPECT_TRUE((std::is_same<decltype(b), std::string&>::value));
-    EXPECT_TRUE((std::is_same<decltype(c), double&     >::value));
+    EXPECT_TRUE((std::is_same<decltype(a), int&   >::value));
+    EXPECT_TRUE((std::is_same<decltype(b), double&>::value));
 
     a = 10;
-    b = "20";
-    c = 34.5;
-    EXPECT_EQ(std::get<0>(tpl), a);
-    EXPECT_EQ(std::get<1>(tpl), b);
-    EXPECT_EQ(std::get<2>(tpl), c);
-}
+    b = 34.5;
 
+    EXPECT_EQ(si::get<0>(tpl), a);
+    EXPECT_EQ(si::get<1>(tpl), b);
+}
+/*
 TEST(si_tuple, equality_operator)
 {
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
-    std::tuple<int, std::string, double> tpl_copy = tpl;
- 
+    si::tuple<int, std::string, double> tpl = si::make_tuple(1, "2", 3.45);
+    si::tuple<int, std::string, double> tpl_copy = tpl;
+
     EXPECT_TRUE(tpl_copy == tpl);
 }
 
 TEST(si_tuple, inequality_operator)
 {
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
-    std::tuple<int, std::string, double> tpl_copy = tpl;
- 
+    si::tuple<int, std::string, double> tpl = si::make_tuple(1, "2", 3.45);
+    si::tuple<int, std::string, double> tpl_copy = tpl;
+
     EXPECT_FALSE(tpl_copy != tpl);
 }
 
 TEST(si_tuple, std_swap)
 {
-    std::tuple<int, std::string, double> tpl = std::make_tuple(1, "2", 3.45);
+    si::tuple<int, std::string, double> tpl = si::make_tuple(1, "2", 3.45);
     auto tpl_copy = tpl;
-    std::get<0>(tpl_copy) = 10;
+    si::get<0>(tpl_copy) = 10;
 
     std::swap(tpl, tpl_copy);
 
-    EXPECT_EQ(std::get<0>(tpl), 10);
-    EXPECT_EQ(std::get<0>(tpl_copy), 1);
+    EXPECT_EQ(si::get<0>(tpl), 10);
+    EXPECT_EQ(si::get<0>(tpl_copy), 1);
 }
-
+*/
