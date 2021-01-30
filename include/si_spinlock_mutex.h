da06d6774a304162196dacfbcf5c0c1b4b8211e0
Alex Marin <alex.marin89@gmail.com>
2021-01-30 15:09:50 -0500

    spinlock optimizations and literature

--- include/si_spinlock_mutex.h
+++ include/si_spinlock_mutex.h
@@ -2,9 +2,12 @@
 #define SI_SPINLOCK_MUTEX_H
 
 #include <atomic>
+#include <immintrin.h> // for _mm_pause
 
 namespace si {
 
+// Notes on why this is a terrible spinlock and why the amd version is 10x faster
+// https://probablydance.com/2019/12/30/measuring-mutexes-spinlocks-and-how-bad-the-linux-scheduler-really-is/
 class spinlock_mutex
 {
 public:
@@ -21,6 +24,34 @@ public:
 
 private:
     std::atomic_flag d_flag = ATOMIC_FLAG_INIT;
+};
+
+// Optimizations based on the AMD slides in the link above
+struct spinlock_amd
+{
+    void lock()
+    {
+        for (;;)
+        {
+            // Loads the memory before attempting to make a change to it.
+            // In the MESI protocol this means that the cache line can be in
+            // the “shared” state on all cores which requires no communication
+            // between the CPU cores until the data actually changes.
+            bool was_locked = locked.load(std::memory_order_relaxed);
+            if (!was_locked && locked.compare_exchange_weak(was_locked, true, std::memory_order_acquire))
+                break;
+            // CPU uses this as a hint that the other hyper-thread running
+            // on the same core should run instead of this thread.
+            _mm_pause();
+        }
+    }
+    void unlock()
+    {
+        locked.store(false, std::memory_order_release);
+    }
+ 
+private:
+    std::atomic<bool> locked{false};
 };
 
 } // namespace si
--- unit_tests/spinlock_mutex_test.cpp
+++ unit_tests/spinlock_mutex_test.cpp
@@ -40,3 +40,38 @@ TEST (SpinlockMutexShould, LockAndUnlock)
 
     EXPECT_EQ(popped_values.size(), num_elems);
 }
+
+TEST (SpinlockAmdShould, LockAndUnlock)
+{
+    std::stack<int> s;
+    const size_t num_elems = 100;
+    for (int i = 0; i < num_elems; i ++)
+        s.push(i);
+
+    si::spinlock_amd m;
+    auto pop = [&m, &s]() {
+        std::unordered_set<int> popped;
+        while (true)
+        {
+            std::unique_lock<std::remove_reference<decltype(m)>::type> ulock(m);
+            if (s.empty())
+                break;
+            int val = s.top();
+            s.pop();
+            ulock.unlock();
+
+            popped.insert(val);
+        }
+
+        return popped;
+    };
+
+    auto f1 = std::async(std::launch::async, pop);
+    auto f2 = std::async(std::launch::async, pop);
+
+    std::unordered_set<int> popped_values;
+    popped_values.merge(f1.get());
+    popped_values.merge(f2.get());
+
+    EXPECT_EQ(popped_values.size(), num_elems);
+}
